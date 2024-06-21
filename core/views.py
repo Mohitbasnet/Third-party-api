ac05ca761e96cb88d4ee83d26b7979372816105e
banset <mohitbasnet80@gmail.com>
2024-06-21 14:46:09 +0545

    added endpoints for github

--- core/views.py
+++ core/views.py
@@ -13,3 +13,11 @@ def home(request):
         'ip': ip_address,
         'country': country_name
     })
+def github(request):
+    user = {}
+    if 'username' in request.GET:
+        username = request.GET['username']
+        url = 'https://api.github.com/users/%s' % username
+        response = requests.get(url)
+        user = response.json()
+    return render(request, 'core/github.html', {'user': user})
